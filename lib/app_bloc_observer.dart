b8ca32361e125f8284070f03bf12c83b5f4cabcf
abdalluhfayez420@gmail.com <77704981+fayez666@users.noreply.github.com>
2023-12-27 05:09:40 +0200

    BloC Observer

---
+++ lib/app_bloc_observer.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class AppBlocObserver extends BlocObserver {
+  @override
+  void onEvent(Bloc bloc, Object? event) {
+    super.onEvent(bloc, event);
+    print('Event: $event');
+  }
+
+  @override
+  void onCreate(BlocBase bloc) {
+    super.onCreate(bloc);
+    print('Create: ${bloc.runtimeType}');
+  }
+
+  @override
+  void onTransition(Bloc bloc, Transition transition) {
+    super.onTransition(bloc, transition);
+    print('Transition: $transition');
+  }
+
+  @override
+  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
+    print('Error: $error');
+    super.onError(bloc, error, stackTrace);
+  }
+}
